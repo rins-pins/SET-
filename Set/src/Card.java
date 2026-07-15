9c27cc1e4adee42410fb13d3e9e6d0cc300bc9be
rins-pins <yeetity751@gmail.com>
2026-07-15 15:47:10 -0400

    Enhance Card.toString() for better readability
    Updated toString method to return descriptive strings for color, shape, and shading.

--- Set/src/Card.java
+++ Set/src/Card.java
@@ -21,7 +21,37 @@ class Card{
         return shading;
     }
     public String toString(){
-        return " " + num + ", " + color + ", " + shape + ", " + shading;
+        String colorString = "";
+        String shapeString = "";
+        String shadingString = "";
+        if(color == 0){
+            colorString = "Red";
+        }
+        else if(color == 1){
+            colorString = "Green";
+        }
+        else{
+            colorString = "Purple";
+        }
+        if(shape == 0){
+            shapeString = "Oval";
+        }
+        else if(shape == 1){
+            shapeString = "Squiggle";
+        }
+        else{
+            shapeString = "Diamond";
+        }
+        if(shading == 0){
+            shadingString = "Filled";
+        }
+        else if(shading == 1){
+            shadingString = "Empty";
+        }
+        else{
+            shadingString = "Lines";
+        }
+        return " Num: " + (num+1) + ", Color: " + colorString + ", Shape: " + shapeString + ", Shading: " + shadingString + "\n";
 
     }
     
